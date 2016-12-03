0b60bd1d2b225619de271e00e59d4574ee89401b
arechap <austin.podoll@gmail.com>
2016-12-03 18:55:27 -0500

    Created PFile class and LockFreeList

---
+++ src/PFile.java
@@ -0,0 +1,35 @@
+/**
+ * Created by Austin on 12/3/2016.
+ */
+public class PFile<T> {
+
+    LockFreeList<Integer> filename = new LockFreeList<>();
+
+    public PFile(String filename) {
+
+    }
+    public void open() {
+
+    }
+
+    public void close() {
+
+    }
+
+    public void read(T content) {
+
+    }
+
+    public void read(T content, int lineNumber) {
+
+    }
+
+    public void write(T content) {
+
+    }
+
+    public void write(T content, int lineNumber) {
+
+
+    }
+}
