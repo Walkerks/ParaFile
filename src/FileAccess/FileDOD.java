fdf0f0e105ac8321c415a9b2fa512042b821383b
Walker Sensabaugh <walkersens@gmail.com>
2016-12-03 16:29:33 -0500

    FileDOD is done and tested

---
+++ src/FileAccess/FileDOD.java
@@ -0,0 +1,69 @@
+package FileAccess;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.FileWriter;
+
+/**
+ * Created by Walker on 12/3/2016.
+ */
+public class FileDOD {
+    File myFile;
+    boolean deleteFileOnGarbage = false;
+    //Opens or creates a new file
+    FileDOD(String fileName){
+        myFile = new File(fileName);
+        if(!myFile.exists()){
+            try {
+                myFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+    //read the file from disk
+    public String read(){
+        String theString = null;
+        try {
+            byte[] encoded = Files.readAllBytes(Paths.get(myFile.getPath()));
+            theString = new String(encoded, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return theString;
+    }
+    //write the file to disk
+    public void write(String content){
+        FileWriter writer;
+
+        try{
+            writer = new FileWriter(myFile);
+            writer.write(content);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Set the file to be deleted when the garbage collector comes along
+    public void delOnGarb(){
+        deleteFileOnGarbage = true;
+    }
+
+    protected void finalize() throws Throwable {
+        try {
+            //If the file is marked to delete
+            if (deleteFileOnGarbage){
+                myFile.delete();
+            }
+        } finally {
+            super.finalize();
+        }
+    }
+
+
+}
---
+++ src/tests/FileDODTest.groovy
@@ -0,0 +1,35 @@
+package tests
+import FileAccess.FileDOD;
+import java.io.File;
+
+/**
+ * Created by Walker on 12/3/2016.
+ */
+class FileDODTest extends groovy.util.GroovyTestCase {
+    String fileName = "testFile.txt";
+
+    void testCreation(){
+        FileDOD myFile = new FileDOD(fileName);
+        File checkFile = new File(fileName);
+        assert(checkFile.exists());
+    }
+    void testWriteRead() {
+        FileDOD myFile = new FileDOD(fileName);
+        String testCont = "sldfkjslk  aaa hfisfs";
+        myFile.write(testCont);
+        assert(testCont.equals(myFile.read()));
+    }
+    void testGarbDelete(){
+        FileDOD myFile = new FileDOD(fileName);
+        myFile.delOnGarb();
+
+        myFile = null;
+        File checkFile = new File(fileName);
+        System.gc();
+        while(checkFile.exists()){
+            //spin
+        }
+    }
+
+
+}
